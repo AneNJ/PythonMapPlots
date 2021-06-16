cf6a6c19b050ab461aac5dd6a5dd3e35de1402d7
anenj <anenjohansen@gmail.com>
2021-06-16 21:17:18 +0200

    Modified to different colors

--- test.py
+++ test.py
@@ -7,7 +7,7 @@ x = np.arange(0,10,0.1)
 for n in np.arange(1,4,1):
     y = np.sin(n*x)
     z = np.cos(n*x)
-    plt.plot(x,y,"r")
-    plt.plot(x,z,"b")
+    plt.plot(x,y,"b")
+    plt.plot(x,z,"y")
 
 plt.show()
